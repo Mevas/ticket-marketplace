fab191f64ca5c32b35ab4ff915d9d99ea16cbb0e
Alexandru <alexandru.star@gmail.com>
2022-08-12 01:14:35 +0300

    feat: add own button component to handle loading

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Button as NextButton,
+  ButtonProps as NextButtonProps,
+  Loading,
+} from "@nextui-org/react";
+
+export type ButtonProps = NextButtonProps & {
+  loading?: boolean | string;
+};
+
+export const Button = ({ loading, children, ...props }: ButtonProps) => {
+  return (
+    <NextButton {...props}>
+      {loading ? (
+        <div style={{ display: "flex", gap: 10, alignItems: "center" }}>
+          <Loading type="spinner" color="currentColor" />
+          {typeof loading === "string" && loading}
+        </div>
+      ) : (
+        children
+      )}
+    </NextButton>
+  );
+};
