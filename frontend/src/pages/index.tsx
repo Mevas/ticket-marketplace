2a2fc2e1b13690d4c17852d8c9da8c93dc47a1e5
Alexandru <alexandru.star@gmail.com>
2022-08-11 13:36:06 +0300

    chore: improve event cards

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from "next";
-import { Container } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Grid,
+  Row,
+  Text,
+} from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import { dehydrate, QueryClient } from "react-query";
 import { Box } from "../components/Box";
@@ -10,10 +18,6 @@ const Account = dynamic<{}>(
   () => import("../components/Account").then((mod) => mod.Account),
   { ssr: false }
 );
-
-const getEvents = async () => {
-  return (await axiosInstance.get("events")).data;
-};
 
 const Home: NextPage = () => {
   const { data } = useEvents();
@@ -29,13 +33,67 @@ const Home: NextPage = () => {
         <Account />
       </Box>
 
-      <div>
-        <ol>
-          {data?.map((event, index) => {
-            return <li key={index}>{event.title}</li>;
-          })}
-        </ol>
-      </div>
+      <Grid.Container gap={2}>
+        {data?.map((event, index) => {
+          return (
+            <Grid xs={3} key={index}>
+              <Card isHoverable isPressable>
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                  <Col>
+                    <Text h3>{event.title}</Text>
+
+                    <Text
+                      size={12}
+                      weight="bold"
+                      transform="uppercase"
+                      color="#222222DD"
+                    >
+                      {event.description}
+                    </Text>
+                  </Col>
+                </Card.Header>
+
+                <Card.Body css={{ p: 0 }}>
+                  <Card.Image
+                    src="https://weraveyou.com/wp-content/uploads/2022/07/Tomorrowland-2022-01.jpg"
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    alt={`${event.title} cover image`}
+                  />
+                </Card.Body>
+
+                <Card.Footer
+                  isBlurred
+                  css={{
+                    position: "absolute",
+                    bgBlur: "#ffffff66",
+                    borderTop:
+                      "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                    bottom: 0,
+                    zIndex: 1,
+                  }}
+                >
+                  <Col>
+                    <Row justify="flex-end">
+                      <Button flat auto rounded color="secondary">
+                        <Text
+                          css={{ color: "inherit" }}
+                          size={12}
+                          weight="bold"
+                          transform="uppercase"
+                        >
+                          Buy tickets
+                        </Text>
+                      </Button>
+                    </Row>
+                  </Col>
+                </Card.Footer>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid.Container>
     </Container>
   );
 };
