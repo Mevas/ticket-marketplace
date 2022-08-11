efe0aba8ea2ba3f5e63db9c2197d201a57b54dd5
Alexandru <alexandru.star@gmail.com>
2022-08-12 01:15:07 +0300

    feat: restrict create event button to logged in users

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,9 +4,11 @@ import { ThemeToggle } from "./ThemeToggle";
 import Link from "next/link";
 import { ConnectButton } from "./ConnectButton";
 import { useIsMounted } from "../hooks/use-is-mounted";
+import { useUser } from "../hooks/useUser";
 
 export const Navbar = () => {
   const isMounted = useIsMounted();
+  const user = useUser();
 
   return (
     <Container lg as="nav" display="flex" wrap="nowrap">
@@ -18,9 +20,11 @@ export const Navbar = () => {
         </Link>
 
         <div style={{ display: "flex", gap: "32px" }}>
-          <Link href="/events/new">
-            <Button rounded>Create event</Button>
-          </Link>
+          {user.isLoggedIn && (
+            <Link href="/events/new">
+              <Button rounded>Create event</Button>
+            </Link>
+          )}
 
           {isMounted && <ConnectButton />}
 
